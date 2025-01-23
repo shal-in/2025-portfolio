0ef5ecce9a613e04976f70e8c5acd2f1ccf5121d
Shalin Ahasan <shalin.a.ahasan@gmail.com>
2025-01-23 13:17:26 +0000

    Finished projects items

--- static/js/index.js
+++ static/js/index.js
@@ -18,6 +18,52 @@ bioCVButtonEl.addEventListener("mouseout", () => {
 })
 
 
+// Projects item buttons
+const projectsItemButtonEls = document.querySelectorAll(".projects .item a");
+projectsItemButtonEls.forEach(projectsItemButtonEl => {
+    projectsItemButtonEl.addEventListener("focus", () => {
+        projectsItemButtonEl.classList.toggle("active");
+    })
+    
+    projectsItemButtonEl.addEventListener("blur", () => {
+        projectsItemButtonEl.classList.toggle("active");
+    })
+    
+    projectsItemButtonEl.addEventListener("mouseover", () => {
+        projectsItemButtonEl.classList.toggle("active");
+    })
+    
+    projectsItemButtonEl.addEventListener("mouseout", () => {
+        projectsItemButtonEl.classList.toggle("active");
+    })
+})
+
+// Projects tech stack
+const projectsSectionEl = document.querySelector("#projects");
+
+const observerCallback = (entries) => {
+    entries.forEach(entry => {
+        // CHANGE THRESHOLDS HERE
+        const isAtLeast45Visible = entry.intersectionRatio >= 0.45;
+        const isLessThan45VisibleAtTop = entry.intersectionRatio < 0.45 && entry.boundingClientRect.top > 0;
+        const isLessThan10VisibleAtBottom = entry.intersectionRatio < 0.45 && entry.boundingClientRect.top < 0;
+
+        if (isAtLeast45Visible) {
+            entry.target.classList.add('active');
+        } else if (isLessThan45VisibleAtTop || isLessThan10VisibleAtBottom) {
+            entry.target.classList.remove('active');
+        }
+    });
+};
+
+const observer = new IntersectionObserver(observerCallback, {
+    root: null,
+    threshold: [0.45, 0.45]
+});
+
+observer.observe(projectsSectionEl);
+
+
 
 // back to top button
 const backToTopButtonEl = document.querySelector(".footer .right .top");
@@ -37,3 +83,4 @@ backToTopButtonEl.addEventListener("focus", () => {
 backToTopButtonEl.addEventListener("blur", () => {
     backToTopButtonEl.classList.toggle("active")
 })
+
